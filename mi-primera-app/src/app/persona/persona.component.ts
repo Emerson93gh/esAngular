2602a4c748abd9e13807577376410117515f426b
Emerson Solano <emerson-solano@hotmail.com>
2022-12-27 17:09:51 -0600

    directiva ngFor y formulario

--- mi-primera-app/src/app/persona/persona.component.ts
+++ mi-primera-app/src/app/persona/persona.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-persona',
@@ -7,12 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonaComponent {
 
-  nombre: string = 'Alexander';
-  apellido: string = 'Solano';
-  private edad: number = 29;
+  //nombre: string = '';
+  //apellido: string = '';
+  // private edad: number = 29;
 
-  getEdad(): number {
-    return this.edad;
+  // getEdad(): number {
+  //   return this.edad;
+  // }
+
+  personas: Persona[] = [new Persona('Alexander', 'Solano'), new Persona('Armando', 'Solano')];
+
+  nombreInput: string = '';
+  apellidoInput: string = '';
+
+  agregarPersona() {
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    this.personas.push( persona1 );
   }
 
  }
