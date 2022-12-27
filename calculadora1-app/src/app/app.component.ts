e777eb859d2005b69c97759b07de0baf8220d48d
Emerson Solano <emerson-solano@hotmail.com>
2022-12-27 15:05:49 -0600

    operaciones aritmeticas

--- calculadora1-app/src/app/app.component.ts
+++ calculadora1-app/src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora1-app';
+  titulo = 'Aplicacion calculadora1';
+  //deshabilitar: string = 'visually-hidden';
+  operandoA: number = 0;
+  operandoB: number = 0;
+  resultado: number = 0;
+  val: string = '';
+
+  sumar():void {
+    this.resultado = this.operandoA + this.operandoB;
+    this.val = "";
+  }
+  restar():void {
+    this.resultado = this.operandoA - this.operandoB;
+    this.val = "";
+  }
+  multiplicar():void {
+    this.resultado = this.operandoA * this.operandoB;
+    this.val = "";
+  }
+  dividir():void {
+    this.resultado = this.operandoA / this.operandoB;
+    this.val = "";
+  }
+  modulo():void {
+    this.resultado = this.operandoA % this.operandoB;
+    if(this.resultado === 0) {
+      this.val = "Par";
+    } else {
+      this.val = "Impar";
+    }
+  }
 }
